25937ad978acbcffaa8519bf8ec3b07b864586ad
Sergei Kleshchenko <kserv2006@yandex.ru>
2022-04-27 00:02:37 +0700

    Junit4 added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,108 +1,109 @@
 package ru.netology.service;
 
-import static org.testng.Assert.assertEquals;
+import org.junit.Test;
 
-class CashbackHackServiceTest {
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
-    void shouldTestUnderZero() {
-        CashbackHackService service = new CashbackHackService();
+    @Test
+    public void shouldTestUnderZero() {
 
         int expected = 1000;
         int actual=service.remain(-1);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    void shouldTestZero() {
+    @Test
+    public void shouldTestZero() {
 
         int expected = 1000;
         int actual=service.remain(0);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    void shouldTestTwo() {
+    @Test
+    public void shouldTestTwo() {
 
         int expected = 998;
         int actual=service.remain(2);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    void shouldTest998() {
+    @Test
+    public void shouldTest998() {
 
         int expected = 2;
         int actual=service.remain(998);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    void shouldTest999() {
+    @Test
+    public void shouldTest999() {
 
         int expected = 1;
         int actual=service.remain(999);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    void shouldTest1000() {
+    @Test
+    public void shouldTest1000() {
 
         int expected = 0;
         int actual=service.remain(1000);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    void shouldTest1001() {
+    @Test
+    public void shouldTest1001() {
 
         int expected = 999;
         int actual=service.remain(1001);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    void shouldTest1002() {
+    @Test
+    public void shouldTest1002() {
 
         int expected = 998;
         int actual=service.remain(1002);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    void shouldTest1998() {
+    @Test
+    public void shouldTest1998() {
 
         int expected = 2;
         int actual=service.remain(1998);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    void shouldTest1999() {
+    @Test
+    public void shouldTest1999() {
 
         int expected = 1;
         int actual=service.remain(1999);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    void shouldTest2000() {
+    @Test
+    public void shouldTest2000() {
 
         int expected = 0;
         int actual=service.remain(2000);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 }
